a802ca8f42f469b6978f08fd57a2564fc449c91e
Mudassir Khan <12mudassarkhan@gmail.com>
2025-07-15 22:45:35 +0530

    completed

---
+++ weather-app/src/InfoBox.jsx
@@ -0,0 +1,41 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
+
+export default function InfoBox({ weatherData }) {
+  const INI_URL =
+    "https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3Vubnl8ZW58MHx8MHx8fDA%3D";
+  const RAIN_URL="https://plus.unsplash.com/premium_photo-1671229652411-4468b946b787?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFpbnxlbnwwfHwwfHx8MA%3D%3D";
+  const HOT_URL="https://images.unsplash.com/photo-1447601932606-2b63e2e64331?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL="https://images.unsplash.com/photo-1519863436079-8436f74be632?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvbGQlMjB3ZWF0aGVyfGVufDB8fDB8fHww";  
+  return (
+    <div className="InfoBox">
+      <div className="InfoBoxContainer">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={weatherData.humidity>80?RAIN_URL: weatherData.temp>15?HOT_URL:COLD_URL}
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {weatherData.city}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "text.secondary" }}
+              component="div"
+            >
+              <div>Temperature:{weatherData.temp}&deg;C</div>
+              <div>Humidity:{weatherData.humidity}</div>
+              <div>Wind Speed:{weatherData.windSpeed}kmph</div>
+              <div>The weather feels like:{weatherData.feelsLike} and it is very good weather in the location</div>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
---
+++ weather-app/src/SearchBox.jsx
@@ -0,0 +1,68 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+import InfoBox from "./InfoBox";
+import Alert from '@mui/material/Alert';
+
+export default function SearchBox() {
+  let [error, setError]= useState(false);
+   let [city, setCity] = useState("");
+   let [weather,setWeather]=useState({});
+  const API_URL= "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY= "8cb1d2b3cd4886f78ead7e6ca1a20cad";
+
+  async function getWeatherData(){
+  
+        let data = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+   if(data.ok){     
+      let jsonData=await data.json();
+      console.log(jsonData)
+      let result={
+        city:city,
+        temp:jsonData.main.temp,
+        humidity:jsonData.main.humidity,
+        windSpeed:jsonData.wind.speed,
+        feelsLike:jsonData.main.feels_like
+      };
+      setWeather(result);
+      setError(false)
+    }else{
+      setError(true);
+      setWeather({});
+    }
+    
+  }
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
+  function preventDefault(event){
+       event.preventDefault();
+    setCity("");
+    getWeatherData();
+  
+  }
+
+  return (
+    <div className="searchBox">
+      <h3>Search for the weather</h3>
+      <form onSubmit={preventDefault}>
+        <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleCity}/>
+        <br></br>
+        <br></br>
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+      <br></br>
+      <br></br>
+      <InfoBox weatherData={weather}/>
+  <br></br>
+  <br></br>
+      {error?<Alert severity="error" style={{position:"absolute",left:"35%"}} >The city you have searched for doesn't exist in the API data</Alert>:""}
+
+    </div>
+  );
+}
